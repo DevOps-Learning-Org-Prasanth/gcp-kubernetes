87b04d94f3669076a665a2e45558977000cb0695
pg185155 <pg185155@ncr.com>
2021-09-20 19:51:14 +0530

    Adding required provider block

--- versions.tf
+++ versions.tf
@@ -5,11 +5,16 @@ terraform {
     bucket = "tfstate-prasanth" # Name of gcp cloud bucket
     prefix = "written-in-state.tf" # Path inside the bucket where state file will be created
   }
+  required_provider {
+    google = {
+      source = "hashicorp/google"
+      version = "=3.84.0"
+    }
+  }
 }
 
 provider "google" {
     project  = "trainingproject-1"
     region   = "us-central1"
     location = "us-central1-c"
-    version = "=3.84.0"
 }
