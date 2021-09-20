1e42b4599b7684e7afffaeca9636a9719b0750dd
pg185155 <pg185155@ncr.com>
2021-09-20 19:53:10 +0530

    fmt changes

--- versions.tf
+++ versions.tf
@@ -1,20 +1,20 @@
 terraform {
-  required_version= "=1.0.2"
-  
+  required_version = "=1.0.2"
+
   backend "gcs" {
-    bucket = "tfstate-prasanth" # Name of gcp cloud bucket
+    bucket = "tfstate-prasanth"    # Name of gcp cloud bucket
     prefix = "written-in-state.tf" # Path inside the bucket where state file will be created
   }
-  required_provider {
+  required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "=3.84.0"
     }
   }
 }
 
 provider "google" {
-    project  = "trainingproject-1"
-    region   = "us-central1"
-    location = "us-central1-c"
+  project  = "trainingproject-1"
+  region   = "us-central1"
+  location = "us-central1-c"
 }
