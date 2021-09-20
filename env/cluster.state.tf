61944f2569356cbfcbe0f541449837c45bd2f17d
pg185155 <pg185155@ncr.com>
2021-09-20 19:35:57 +0530

    Adding actions and initial setup for terraform

---
+++ env/cluster.state.tf
@@ -0,0 +1,1 @@
+prefix = "manual-cluster/state"
--- versions.tf
+++ versions.tf
@@ -1,7 +1,15 @@
 terraform {
+  required_version= "=1.0.2"
+  
+  backend "gcs" {
+    bucket = "tfstate-prasanth" # Name of gcp cloud bucket
+    prefix = "written-in-state.tf" # Path inside the bucket where state file will be created
+  }
+
   provider "google" {
     project  = "trainingproject-1"
     region   = "us-central1"
     location = "us-central1-c"
+    version = "=3.84.0"
   }
 }
