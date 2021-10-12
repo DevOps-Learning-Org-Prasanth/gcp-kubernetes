27750aac6c2ce29fa51503ab8651a7c83ea5daed
Prasanth-518 <prasanthvg3@gmail.com>
2021-10-12 21:47:58 +0530

    adding files for modules

--- main.tf
+++ main.tf
@@ -1,7 +1,22 @@
 # vpc
+module "vpc" {
+  source = "./modules/network_subnet"
+}
 
 # loadbalancer
+module "loadbalancer" {
+  source = "./modules/loadbalancer"
+}
 
 # master node
+module "master_node" {
+  source   = "./modules/linux_servers"
+  tiercode = "mtr"
+}
 
 # worker nodes
+module "worker_node" {
+  source   = "./modules/linux_servers"
+  tiercode = "wkr"
+  count    = 2
+}
