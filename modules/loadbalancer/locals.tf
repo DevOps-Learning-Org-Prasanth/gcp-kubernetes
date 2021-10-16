b9cbf7322a1b071f7fc657796bd9cd4377691217
Prasanth-518 <prasanthvg3@gmail.com>
2021-10-16 14:13:31 +0530

    adding port range for forwarding rule

--- modules/loadbalancer/locals.tf
+++ modules/loadbalancer/locals.tf
@@ -1,4 +1,4 @@
 locals {
-    port_ranges = split(var.ports[0],"-")
-    health_check_ports = [for port in range(tonumber(port_ranges[0]),tonumber(port_ranges[1])): tostring(port)]
+  port_ranges        = split(var.ports[0], "-")
+  health_check_ports = [for port in range(tonumber(port_ranges[0]), tonumber(port_ranges[1])) : tostring(port)]
 }
