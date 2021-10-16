e5b225348724001fda374ce5a493125f45fcd381
Prasanth-518 <prasanthvg3@gmail.com>
2021-10-16 19:51:30 +0530

    Adding egress priorities

--- modules/firewall/variables.tf
+++ modules/firewall/variables.tf
@@ -1,24 +1,27 @@
 variable "firewalls" {
   type = list(object({
-    name     = string
-    ports    = list(string)
-    tags     = string
-    priority = number
+    name             = string
+    ports            = list(string)
+    tags             = string
+    ingress_priority = number
+    egress_priority  = number
   }))
   description = <<-DESC
     Input should be in following manner
-    components = [
+    firewalls = [
       {
         name : "master",,
         ports : ["22","33","100-10000"],
         tags : "master_node",
-        priority : 1000,
+        ingress_priority : 1000,
+        egress_priority : 1000,
       },
       {
         name : "worker",
         ports: ["7852","5454","10002-10025"],
         tags : "worker_node",
-        priority : 1001.
+        ingress_priority : 1000,
+        egress_priority : 1000,
       },
     ]
   DESC
--- variables.tf
+++ variables.tf
@@ -15,10 +15,11 @@ variable "subnets" {
 
 variable "firewalls" {
   type = list(object({
-    name     = string
-    ports    = list(string)
-    tags     = string
-    priority = number
+    name             = string
+    ports            = list(string)
+    tags             = string
+    ingress_priority = number
+    egress_priority  = number
   }))
   description = <<-DESC
     Input should be in following manner
@@ -27,13 +28,15 @@ variable "firewalls" {
         name : "master",,
         ports : ["22","33","100-10000"],
         tags : "master_node",
-        priority : 1000,
+        ingress_priority : 1000,
+        egress_priority : 1000,
       },
       {
         name : "worker",
         ports: ["7852","5454","10002-10025"],
         tags : "worker_node",
-        priority : 1001
+        ingress_priority : 1000,
+        egress_priority : 1000,
       },
     ]
   DESC
