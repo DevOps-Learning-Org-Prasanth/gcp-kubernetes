818c2ed8f7d3fa19a7b47986bcd435915190b1eb
Prasanth-518 <prasanthvg3@gmail.com>
2021-10-16 16:20:16 +0530

    Removing health check

--- modules/loadbalancer/locals.tf
+++ modules/loadbalancer/locals.tf
@@ -1,4 +1,4 @@
-locals {
-  port_ranges        = split("-", var.ports[0])
-  health_check_ports = [for port in range(tonumber(local.port_ranges[0]), tonumber(local.port_ranges[1]+1)) : tostring(port)]
-}
+# locals {
+  # port_ranges        = split("-", var.ports[0])
+  # health_check_ports = [for port in range(tonumber(local.port_ranges[0]), tonumber(local.port_ranges[1]+1)) : tostring(port)]
+# }
