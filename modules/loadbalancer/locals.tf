e877303a3e67493c10352131478235d46d38e214
Prasanth-518 <prasanthvg3@gmail.com>
2021-10-16 16:25:21 +0530

    Updating instances for target pool

--- modules/loadbalancer/locals.tf
+++ modules/loadbalancer/locals.tf
@@ -1,4 +1,3 @@
-# locals {
-  # port_ranges        = split("-", var.ports[0])
-  # health_check_ports = [for port in range(tonumber(local.port_ranges[0]), tonumber(local.port_ranges[1]+1)) : tostring(port)]
-# }
+locals {
+  instances = [for instance in var.instances: "${var.zone}/${instance}"]
+}
