667f49ec8812a147453e0e2b185c7f3472cd57fb
Prasanth-518 <prasanthvg3@gmail.com>
2021-10-18 17:43:22 +0530

    Changing scope of nat to all subnets in the vpc

--- modules/network_subnet/main.tf
+++ modules/network_subnet/main.tf
@@ -21,11 +21,7 @@ resource "google_compute_router" "main" {
 resource "google_compute_router_nat" "main" {
   name                               = "${var.region}-${google_compute_network.main.name}-nat-gateway"
   nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGE"
   router                             = google_compute_router.main.name
   region                             = var.region
-  subnetwork {
-    name                    = google_compute_subnetwork.main.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  }
 }
